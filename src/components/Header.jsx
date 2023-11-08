81a81929e4724d5bcb10eb49087af29cdb5b42d2
azharanggakusuma <azharanggakusuma01@gmail.com>
2023-11-08 10:27:47 +0700

    Perbaikan tampilan header dan penambahan icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,87 +1,36 @@
 // Header.js
-import React from "react";
+import React, { useState } from "react";
+import { FaSearch, FaSignInAlt } from "react-icons/fa";
 
 const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <header className="bg-black text-white py-4 px-8">
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold">Browse</h1>
-        <div className="flex space-x-4">
-          <a href="#" className="hover:text-gray-400">
-            Home
-          </a>
-          <a href="#" className="hover:text-gray-400">
-            Search
-          </a>
-          <a href="#" className="hover:text-gray-400">
-            Your Library
-          </a>
+        <div className="flex items-center space-x-4">
+          <div className="relative">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              placeholder="Search..."
+              className="bg-gray-800 text-white rounded-full py-2 px-4 pl-10"
+            />
+            <div className="absolute left-3 top-2 text-gray-500">
+              <FaSearch />
+            </div>
+          </div>
         </div>
         <div className="flex space-x-4">
-          <a href="#" className="hover:text-gray-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </a>
-          <a href="#" className="hover:text-gray-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </a>
-          <a href="#" className="hover:text-gray-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </a>
-          <a href="#" className="hover:text-gray-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </a>
+          <button className="bg-white hover:bg-gray-200 text-black text-sm font-semibold rounded-full py-3 px-6">
+            Log In
+          </button>
         </div>
       </div>
     </header>
