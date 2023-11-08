07aa26988e5506011b0f3422d12c2f77b2afabd6
azharanggakusuma <azharanggakusuma01@gmail.com>
2023-11-08 10:33:52 +0700

    Update Title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header className="bg-black text-white py-4 px-8">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Browse</h1>
+        <h1 className="text-2xl font-bold">Spotify Clone</h1>
         <div className="relative">
           <div className="absolute inset-y-0 left-3 flex items-center text-gray-500">
             <FaSearch />
