5f88084499de39ac66fae182a318b83fba86b0bf
azharanggakusuma <azharanggakusuma01@gmail.com>
2023-11-08 10:14:44 +0700

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import React from "react";
+import Header from "./components/Header";
+import PlaylistGrid from "./components/PlaylistGrid";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-gray-900 text-white min-h-screen">
+      <Header />
+      <main className="container mx-auto p-8">
+        <h1 className="text-3xl font-semibold mb-4">Featured Playlists</h1>
+        <PlaylistGrid />
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
